00c0b0c5464d89304b29fac83e35d04d890bfd40
Renz <calangi.renz@gmail.com>
2018-08-22 22:29:32 +0800

    update test case

--- robot_factory/robots/tests/test_models.py
+++ robot_factory/robots/tests/test_models.py
@@ -6,7 +6,7 @@ from .factories import robot
 from ..models import Robot
 
 
-def RobotModelTestCase(TestCase):
+class RobotModelTestCase(TestCase):
     """
     Defines the test suites to run against the Robot model.
     """
@@ -18,13 +18,28 @@ def RobotModelTestCase(TestCase):
         robot = mixer.blend('robot.Robot')
         self.assertTrue(robot.pk is not None)
 
+    def test_model_cant_create_robot_with_invalid_configuration(self):
+        self.assertRaises(ValueError, mixer.blend(
+            'robot.Robot',
+            categories={'hasPlug': True}
+        ))
+
+    def test_model_cant_create_robot_with_invalid_qa_status(self):
+        robot = mixer.blend('robot.Robot', qa_status='invalid--')
+
     def test_model_string_returns_robot_name(self):
         self.assertEqual(str(self.robot), self.robot.name)
 
     def test_model_is_recyclable_property(self):
         robot = mixer.blend(
             'robot.Robot',
-            categories={'hasWheels': True, 'hasTracks': True}
+            categories={
+                'hasSentience': False,
+                'hasWheels': True,
+                'hasTracks': True,
+                'numberOfRotors': 0,
+                'color': '',
+            }
         )
         
         self.assertTrue(robot.is_recyclable)
