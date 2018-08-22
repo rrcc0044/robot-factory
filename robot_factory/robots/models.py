722ea14b0bfd59803c6a1513f2ef0d55bab5fc14
Renz <calangi.renz@gmail.com>
2018-08-22 23:31:33 +0800

    :tada: create initial Robot & Status model

--- robot_factory/robots/models.py
+++ robot_factory/robots/models.py
@@ -1,5 +1,63 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
+def _default_configuration_fields():
+    return {
+        'hasSentience': False,
+        'hasWheels': False,
+        'hasTracks': False,
+        'numberOfRotors': 0,
+        'color': '',
+    }
+
+
 class Robot(models.Model):
-    pass
+    QA_STATUS = (
+        ('factory_seconds', 'Factory Seconds'),
+        ('passed_qa', 'Passed QA'),
+    )
+
+    name = models.CharField(max_length=255)
+    configuration = JSONField(default=_default_configuration_fields)
+    qa_status = models.CharField(max_length=20, choices=QA_STATUS, null=True)
+
+    @property
+    def is_recyclable(self):
+        rotors = self.configuration['numberOfRotors']
+        wheels = self.configuration['hasWheels']
+        color = self.configuration['color']
+        tracks = self.configuration['hasTracks']
+
+        if rotors:
+            if color == 'blue' or (rotors < 3 or rotors > 8):
+                return True
+
+        if wheels:
+            if tracks:
+                return True
+
+        return False
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        required_configuration = set(_default_configuration_fields())
+        print(self.configuration)
+        print(required_configuration)
+        if not set(self.configuration).issubset(required_configuration):
+            raise ValueError('configuration not allowed')
+
+        if not any(self.qa_status in code for code in self.QA_STATUS):
+            raise ValueError('qa_status not allowed')
+
+        super().save(*args, **kwargs)
+
+
+class Status(models.Model):
+    robots = models.ManyToManyField(Robot, related_name='status')
+    text = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.text
