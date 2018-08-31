5c8001434e1b29dd2ec01766fa91f42376bd045b
Renz <calangi.renz@gmail.com>
2018-08-31 13:28:04 +0800

    fix robot app broken test

--- robot_factory/robots/tests/test_views.py
+++ robot_factory/robots/tests/test_views.py
@@ -1,10 +1,9 @@
 from django.urls import reverse
 from mixer.backend.django import mixer
-from rest_framework.views import reverse_action
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-from ..models import Robot
+from ..models import Robot, Status
 
 
 class RobotViewTestCase(APITestCase):
@@ -13,7 +12,7 @@ class RobotViewTestCase(APITestCase):
     """
 
     def setUp(self):
-        self.robot = mixer.blend('robots.Robot')
+        self.robot = mixer.blend('robots.Robot', qa_status=None)
 
     def test_get_request_returns_robot_list_for_qa(self):
         url = reverse('robot-list')
@@ -21,20 +20,49 @@ class RobotViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_extinguish_updates_a_robot(self):
-        url = reverse_action('extinguish', args=[str(self.robot.id)])
+        robot = mixer.blend(
+            'robots.Robot',
+            configuration={
+                'hasSentience': True,
+                'hasWheels': True,
+                'hasTracks': True,
+                'numberOfRotors': 0,
+                'color': '',
+            },
+            qa_status=None
+        )
+
+        on_fire_status, _ = Status.objects.get_or_create(text='on fire')
+        robot.status.add(on_fire_status)
+
+        url = f"{reverse('robot-list')}/{robot.id}/extinguish"
         response = self.client.put(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        robot = Robot.objects.get(pk=self.robot.id)
+        robot = Robot.objects.get(pk=robot.id)
         self.assertFalse(robot.status.filter(text='on fire'))
 
     def test_recycle_deletes_a_robot(self):
-        robot = mixer.blend('robots.Robot')
-        url = reverse_action('recycle')
-        payload = {'recycleRobots': [robot.id]}
+        robot = mixer.blend(
+            'robots.Robot',
+            configuration={
+                'hasSentience': False,
+                'hasWheels': True,
+                'hasTracks': True,
+                'numberOfRotors': 0,
+                'color': '',
+            },
+            qa_status=None
+        )
 
-        response = self.client.delete(url, data=payload)
+        url = reverse('robot-list') + '/recycle'
+
+        body = {
+            'recycleRobots': [robot.id]
+        }
+
+        response = self.client.delete(url, body, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         with self.assertRaises(Robot.DoesNotExist):
-            Robot.objects.get(pk=robot.id)
+            robot = Robot.objects.get(id=robot.id)
